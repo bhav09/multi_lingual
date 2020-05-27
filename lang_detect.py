41777995c62e4265cf037cb007b266f388e0fd05
Bhavishya Pandit <50860603+bhav09@users.noreply.github.com>
2020-05-27 19:28:40 +0530

    Add files via upload

---
+++ lang_detect.py
@@ -0,0 +1,9 @@
+from pycountry import languages
+from langdetect import detect_langs , detect
+
+def d():
+    ddd=detect(text)
+    print(ddd)
+text = input('Enter text to detect language:')
+#print(detect_langs(text))
+d()
